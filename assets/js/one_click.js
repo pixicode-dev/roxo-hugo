9def4a7cdf2a9f0d9694a775da660b2eceb99103
PixiCode <contact@pixicode.dev>
2024-11-07 09:57:40 +0100

    carter

--- assets/js/one_click.js
+++ assets/js/one_click.js
@@ -1,11 +1,11 @@
-document.getElementById("itemClickId").addEventListener("click", function(event) {
-    var target = event.target.closest(".site-project-item");
-    console.log(target)
-    var linkElement = target.getElementsByTagName("a")[0]; // Premier lien
+document.querySelectorAll("#itemClickId").forEach(function(item) {
+    item.addEventListener("click", function(event) {
+        var target = event.target.closest(".site-project-item");
+        var linkElement = target.getElementsByTagName("a")[0]; // Premier lien
 
-    console.log(linkElement.href)
-    // Redirige l'utilisateur vers une page HTTPS
-window.location.href = linkElement.href;
-
+        // Redirige l'utilisateur vers une page HTTPS
+        window.location.href = linkElement.href;
+    });
 });
+
 
