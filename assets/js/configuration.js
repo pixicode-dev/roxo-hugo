53f4f1ca833d6308c4285a91953c23776213876c
Emric Pichonnier <emric.pichonnier@gmail.com>
2025-10-24 12:51:39 +0200

    fix: cookiebot loaccaly

---
+++ assets/js/configuration.js
@@ -0,0 +1,33 @@
+CookieConsent.configuration.tags.push({
+  id: 168017013,
+  type: "script",
+  tagID: "",
+  innerHash: "",
+  outerHash: "",
+  tagHash: "11013912564335",
+  url: "https://pixicode.dev/uc.js",
+  resolvedUrl: "https://pixicode.dev/uc.js",
+  cat: [1],
+});
+CookieConsent.configuration.tags.push({
+  id: 168017014,
+  type: "script",
+  tagID: "",
+  innerHash: "",
+  outerHash: "",
+  tagHash: "3765595810080",
+  url: "https://www.google.com/recaptcha/api.js",
+  resolvedUrl: "https://www.google.com/recaptcha/api.js",
+  cat: [1],
+});
+CookieConsent.configuration.tags.push({
+  id: 168017015,
+  type: "script",
+  tagID: "",
+  innerHash: "",
+  outerHash: "",
+  tagHash: "3260306921669",
+  url: "",
+  resolvedUrl: "",
+  cat: [1],
+});
